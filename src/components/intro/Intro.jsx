20bdbe4c14754893c4d94e91cf37d923a6a06d4c
hawar <hawar-sevar@hotmail.com>
2021-11-03 11:31:54 +0100

    change type

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -24,7 +24,7 @@ export default function Intro() {
       </div>
       <div className="right">
         <div className="wrapper">
-          <h2>Hi There, I'm</h2>
+          <h2>Hello, I'm</h2>
           <h1>Hawar A.</h1>
           <h3>
             Freelance <span ref={textRef}></span>
